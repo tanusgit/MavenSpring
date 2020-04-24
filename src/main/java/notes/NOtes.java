0cbb0c16565c56910c51dd437272194f05ac78c1
tanu <24976766+tanusgit@users.noreply.github.com>
2020-04-24 11:53:32 -0700

    notes emp

---
+++ src/main/java/notes/NOtes.java
@@ -0,0 +1,66 @@
+package notes;
+
+public class NOtes {
+/*
+ * Emp application:
+
+Users : Admin and Employee
+
+Admin Roles:
+----------------------------------------------------------------------
+1. Profile mgmt  
+  a) see his info (personal info + present add + permanent addr )
+  b)Update his info.  (personal info + present add + permanent addr )
+if manager then show sub ordinates
+  c) Change the password (encryption of the password in the database)
+2. create new employee
+a) create new employee + 
+     designation [list] +
+	 depart name [list] +
+	 manger name [list]
+b) serach by name and serach by empId
+c) adv serach employee + pagination
+d) show all emps + pagination + sorting the data 
+
+3. Create department
+a) specify dept name + manager name 
+b)search department and show all emps under dept
+c)show all depts with emps in the dept
+4 .Update depratment
+5. Delete department
+ ,IF THERE ARE ANY EMPS under depart then dont delete dept , 
+ throw error msg
+6) password reset 
+7) Admin approval for status active, inactive , disabled, blocked.
+8) Logout
+
+
+
+Employee Role:
+----------------------------
+1. Profile mgmt  
+  a) see his info (personal info + present add + permanent addr )
+  b)Update his info.  (personal info + present add + permanent addr )
+  c) Change the password (encryption of the password in the database)
+  
+2. Search employee
+a) serach by name and serach by empId
+b) adv serach employee + pagination
+
+if manager , then show all emps reporting to u
+
+3. if the employee is a manager :
+then show the employees who are reporting to the manager
+
+4. Logout
+  
+  
+ 
+ Login screen:
+ ----------------
+ if the password is entered more than three times then account will be blocked and should contact admin.
+ if password expairy period is 3 months then should change the password in every 3 months.
+ 
+ 
+ */
+}
